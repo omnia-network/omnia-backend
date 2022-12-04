009e6b97599f2a5b8d2a83fd7d8ab9eb52859108
massimoalbarello <massimo00albarello@gmail.com>
2022-12-04 21:43:10 +0000

    deploy and use II locally

--- omnia_main/src/omnia_main_backend/src/lib.rs
+++ omnia_main/src/omnia_main_backend/src/lib.rs
@@ -1,4 +1,12 @@
+use candid::Principal;
+use ic_cdk::api;
+
 #[ic_cdk_macros::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
+}
+
+#[ic_cdk_macros::query]
+fn whoami() -> Principal {
+    api::caller()
 }
--- omnia_main/src/omnia_main_frontend/src/App.tsx
+++ omnia_main/src/omnia_main_frontend/src/App.tsx
@@ -1,12 +1,14 @@
-import React, { FormEvent, useRef, useState } from 'react';
+import React, { FormEvent, useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { omnia_main_backend } from "../../declarations/omnia_main_backend";
+import { getAuthClient } from './services/authClient';
 
 const App = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [result, setResult] = useState<string>('');
   const inputRef = useRef<HTMLInputElement>(null);
+  const [isLogged, setIsLogged] = useState(false);
 
   const onFormSumbit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -28,20 +30,50 @@ const App = () => {
     }
     
     setIsSubmitting(false);
-
   };
+
+  const handleLogin = async () => {
+    const authClient = await getAuthClient();
+    authClient.login({
+      async onSuccess() {
+        console.log("You are logged in as", await omnia_main_backend.whoami());
+        setIsLogged(true);
+      },
+      onError(error) {
+        console.log(error);
+      },
+      identityProvider: `http://localhost:4943?canisterId=${process.env.INTERNET_IDENTITY_CANISTER_ID}`
+    });
+  }
+
+  useEffect(() => {
+    getAuthClient().then(async (authClient) => {
+      console.log("Auth Client initialized", authClient);
+      const isAuth = await authClient.isAuthenticated();
+      if (isAuth) {
+        console.log("You are logged in as", await omnia_main_backend.whoami());
+      }
+      setIsLogged(isAuth);
+    });
+  }, []);
 
   return (
     <div className="App">
-      <img src={logo} alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={onFormSumbit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input ref={inputRef} id="name" alt="Name" type="text" />
-        <button type="submit" disabled={isSubmitting}>Click Me!</button>
-      </form>
-      <section id="greeting">{result}</section>
+      {isLogged 
+        ? <div>
+          <img src={logo} alt="DFINITY logo" />
+          <br />
+          <br />
+          <form action="#" onSubmit={onFormSumbit}>
+            <label htmlFor="name">Enter your name: &nbsp;</label>
+            <input ref={inputRef} id="name" alt="Name" type="text" />
+            <button type="submit" disabled={isSubmitting}>Click Me!</button>
+          </form>
+          <section id="greeting">{result}</section>
+        </div> 
+        : <div>
+          <button onClick={handleLogin}>Login</button>
+        </div>}
     </div>
   );
 }
