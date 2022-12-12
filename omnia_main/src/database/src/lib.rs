ce0ee366ec53d2c7b9552d2ca8eccc7e35b61902
massimoalbarello <massimo00albarello@gmail.com>
2022-12-12 18:23:32 +0100

    set user in environment by uid

--- omnia_main/src/database/src/lib.rs
+++ omnia_main/src/database/src/lib.rs
@@ -5,12 +5,14 @@ use std::cell::RefCell;
 use std::collections::BTreeMap;
 
 //  ENVIRONMENTS DATABASE
-type EnvironmentStore = BTreeMap<Principal, EnvironmentInfo>;
+type EnvironmentUID = String;
+type EnvironmentStore = BTreeMap<EnvironmentUID, EnvironmentInfo>;
 
-#[derive(Debug, CandidType)]
+#[derive(Debug, Clone, CandidType, Deserialize)]
 struct EnvironmentInfo {
     pub env_name: String,
     pub env_uid: String,
+    pub env_manager_principal_id: String,
 }
 
 #[derive(Debug, CandidType, Deserialize)]
@@ -45,23 +47,23 @@ fn initialize_new_environment(
     environment_registration_input: EnvironmentRegistrationInput
 ) -> ManualReply<EnvironmentRegistrationResult>{
 
-    let environment_manager_principal = Principal::from_text(environment_manager_principal_id).unwrap();
-    ic_cdk::print(format!("Initializing environment: {:?} managed by: {:?}", environment_registration_input, environment_manager_principal));
-    
+    ic_cdk::print(format!("Initializing environment: {:?} managed by: {:?}", environment_registration_input, environment_manager_principal_id));
+
     ENVIRONMENT_STORE.with(|environment_store| {
         environment_store.borrow_mut().insert(
-            environment_manager_principal,
+            String::from("1234567890"),
             EnvironmentInfo {
-                env_name: String::from("Random environment name"),
-                env_uid: String::from("Random environment UID"),
+                env_name: environment_registration_input.env_name,
+                env_uid: String::from("1234567890"),
+                env_manager_principal_id: environment_manager_principal_id,
             }
         );
     });
-    
+
     let environment_registration_result = EnvironmentRegistrationResult {
-        env_uid: String::from("Random environment UID"),
+        env_uid: String::from("1234567890"),
     };
-    
+
     ManualReply::one(environment_registration_result)
 }
 
@@ -74,29 +76,39 @@ fn set_user_in_environment(user_principal_id: String, env_uid: String) -> Manual
 
     match get_user_profile_if_exists(user_principal) {
         Some(user_profile) => {
-            let updated_user_profile = UserProfile {
-                environment_uid : Some(env_uid.clone()),
-                ..user_profile
-            };
+            match get_environment_info_by_uid(&env_uid) {
+                Some(environment_info) => {
+                    let updated_user_profile = UserProfile {
+                        environment_uid : Some(env_uid.to_owned()),
+                        ..user_profile
+                    };
 
-            USER_PROFILE_STORE.with(|profile_store| {
-                profile_store.borrow_mut().insert(user_principal, updated_user_profile);
-            });
+                    USER_PROFILE_STORE.with(|profile_store| {
+                        profile_store.borrow_mut().insert(user_principal, updated_user_profile);
+                    });
 
-            ic_cdk::print(format!("User: {:?} set in environment with UID: {:?}", user_principal, env_uid));
+                    ic_cdk::print(format!("User: {:?} set in environment: {:?}", user_principal, environment_info));
 
-            ManualReply::one(EnvironmentInfo {
-                env_name: String::from("Fake environment name"),
-                env_uid,
-            })
-
+                    ManualReply::one(environment_info)
+                },
+                None => panic!("Environment does not exist"),
+            }
         },
-        None => {
-            panic!("User does not have a profile")
-        }
+        None => panic!("User does not have a profile")
     }
-
 }
+
+
+
+fn get_environment_info_by_uid(environment_uid: &EnvironmentUID) -> Option<EnvironmentInfo> {
+    ENVIRONMENT_STORE.with(|environment_store| {
+        match environment_store.borrow().get(environment_uid) {
+            Some(environment_info) => Some(environment_info.to_owned()),
+            None => None,
+        }
+    })
+}
+
 
 
 #[ic_cdk_macros::update(name = "getUserProfile", manual_reply = true)]
@@ -117,7 +129,7 @@ fn get_user_profile(user_principal_id: String) -> ManualReply<UserProfile> {
                 user_principal_id: user_principal.to_string(),
                 environment_uid: None,
             };
-    
+
             ic_cdk::print(format!("Created profile: {:?} of user: {:?}", new_user_profile, user_principal));
 
             USER_PROFILE_STORE.with(|profile_store| {
@@ -128,6 +140,8 @@ fn get_user_profile(user_principal_id: String) -> ManualReply<UserProfile> {
         }
     }
 }
+
+
 
 fn get_user_profile_if_exists(user_principal: Principal) -> Option<UserProfile> {
     USER_PROFILE_STORE.with(|profile_store| {
