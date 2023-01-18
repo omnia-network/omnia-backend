b78452e8c54c90a75dc00f26213b31beea1dcc96
massimoalbarello <massimo00albarello@gmail.com>
2023-01-18 11:58:22 +0100

    remove registered gateway from initialized store

--- omnia_main/src/omnia_backend/src/manager.rs
+++ omnia_main/src/omnia_backend/src/manager.rs
@@ -11,6 +11,7 @@ pub struct Database;
 async fn create_environment(
     environment_creation_input: EnvironmentCreationInput
 ) -> Box<EnvironmentCreationResult> {
+
     let environment_manager_principal = api::caller();
 
     let environment_creation_result = Database::createNewEnvironment(
@@ -27,8 +28,8 @@ async fn create_environment(
 
 #[ic_cdk_macros::update(name = "initGateway")]
 fn init_gateway() -> String {
-    let gateway_uuid = generate_uuid();
 
+    let gateway_uuid = generate_uuid();
     ic_cdk::print(format!("Initialized gateway with UUID: {:?}", gateway_uuid));
 
     INITIALIZED_GATEWAY_STORE.with(|intialized_gateway_store| {
@@ -48,7 +49,11 @@ async fn register_gateway(
     let environment_manager_principal = api::caller();
 
     let is_initialized = INITIALIZED_GATEWAY_STORE.with(|intialized_gateway_store| {
-        intialized_gateway_store.borrow().contains(&gateway_registration_input.gateway_uid)
+        let mut store = intialized_gateway_store.borrow_mut();
+        match store.contains(&gateway_registration_input.gateway_uid) {
+            true => store.remove(&gateway_registration_input.gateway_uid),
+            false => false
+        }
     });
 
     if is_initialized {
@@ -61,6 +66,7 @@ async fn register_gateway(
         
         return Some(gateway_registration_result);
     }
+    ic_cdk::print("Could not register gateway as it is not initialized");
     None
 }
 
